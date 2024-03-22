d18c4d0b376de41f4336a86ab2556b454e90e067
Elena Koroleva <berries.whale@gmail.com>
2024-03-22 14:01:14 +0300

    init: start async-race project and scaffold

---
+++ async-race/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'max-lines-per-function': ['warn', 40],
+  },
+};
