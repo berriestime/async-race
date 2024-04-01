251c25022122cd25a29babcb75d458c1189d6650
Elena Koroleva <berries.whale@gmail.com>
2024-04-01 17:18:26 +0300

    fix: proper root style import

--- async-race/src/main.ts
+++ async-race/src/main.ts
@@ -1,7 +1,7 @@
 import initFooter from './components/footer/footer';
 import { initHeader } from './components/header/header';
 import { initGarage } from './pages/garage/garage-page';
-import './style.module.css';
+import './style.css';
 
 initHeader();
 initGarage();
